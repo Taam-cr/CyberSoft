602b80b2d657c683edb3ec81a9f5dca47658951d
Taam-cr <tranphanthaotam2003@gmail.com>
2025-10-17 03:48:12 +0700

    Tinh tong

--- VongLap/src/While.java
+++ VongLap/src/While.java
@@ -1,23 +1,33 @@
 import java.util.Scanner;
 
 public class While {
+
+    public static double tinhTong(int n, double x) {
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += Math.pow(x, i); // x^i
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;
+        double x;
+
         do {
-            System.out.print("Nhập số nguyên dương lớn hơn 0: ");
+            System.out.print("Vui lòng nhập số n vào: ");
             n = sc.nextInt();
             if (n <= 0) {
-                System.out.println("Vui lòng nhập lại");
+                System.out.println("n phải là số nguyên dương, vui lòng nhập lại!");
             }
         } while (n <= 0);
-        int sum = 0;
-        for (int i = 1; i < n; i++) {
-            if (i % 2 != 0) {
-                sum += i;
-            }
-        }
 
-        System.out.println("Tổng các số bé hơn " + n + " là: " + sum);
+        System.out.print("Vui lòng nhập số x vào: ");
+        x = sc.nextDouble();
+
+        double result = tinhTong(n, x);
+
+        System.out.println("Tổng của n = " + n + " và x = " + x + " là: " + result);
     }
 }
