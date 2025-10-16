ef4f3d6c1fa63797cabb9087f12c3c74f279c376
Taam-cr <tranphanthaotam2003@gmail.com>
2025-10-17 03:58:10 +0700

    ve tam giac

--- VongLap/src/While.java
+++ VongLap/src/While.java
@@ -4,23 +4,46 @@ public class While {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Vui lòng nhập số tiền gửi (triệu) vào: ");
-        double tienGoc = sc.nextDouble();
+        System.out.print("Nhập chiều cao tam giác n: ");
+        int n = sc.nextInt();
 
-        System.out.print("Vui lòng nhập số tiền mong muốn (triệu) vào: ");
-        double tienMongMuon = sc.nextDouble();
+        System.out.println("\nTam giác cân đặc:");
+        inTamGiacCanDac(n);
 
-        System.out.print("Vui lòng nhập lãi suất gửi (%) từ 0 - 100 vào: ");
-        double laiSuat = sc.nextDouble();
+        System.out.println("\nTam giác cân rỗng:");
+        inTamGiacCanRong(n);
+    }
 
-        int nam = 0;
-        double tienHienTai = tienGoc;
-
-        while (tienHienTai < tienMongMuon) {
-            tienHienTai += tienHienTai * laiSuat / 100; 
-            nam++;
+    // Hàm in tam giác cân 
+    public static void inTamGiacCanDac(int n) {
+        for (int i = 1; i <= n; i++) {
+            // in khoảng trắng
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // in dấu *
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
+    }
 
-        System.out.println("Số năm đợi chờ là: " + nam);
+    // Hàm in tam giác cân rỗng
+    public static void inTamGiacCanRong(int n) {
+        for (int i = 1; i <= n; i++) {
+            // in khoảng trắng đầu dòng
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // in các ký tự của hàng
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (i == n || j == 1 || j == 2 * i - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
