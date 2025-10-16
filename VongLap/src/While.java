f78860cf11792c661d544ad414ebfed071bccdd8
Taam-cr <tranphanthaotam2003@gmail.com>
2025-10-17 03:52:13 +0700

    Giai thua

--- VongLap/src/While.java
+++ VongLap/src/While.java
@@ -2,32 +2,21 @@ import java.util.Scanner;
 
 public class While {
 
-    public static double tinhTong(int n, double x) {
-        double sum = 0;
+    public static long tinhGiaiThua(int n) {
+        long giaiThua = 1;
         for (int i = 1; i <= n; i++) {
-            sum += Math.pow(x, i); // x^i
+            giaiThua *= i;
         }
-        return sum;
+        return giaiThua;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
-        double x;
 
-        do {
-            System.out.print("Vui lòng nhập số n vào: ");
-            n = sc.nextInt();
-            if (n <= 0) {
-                System.out.println("n phải là số nguyên dương, vui lòng nhập lại!");
-            }
-        } while (n <= 0);
+        System.out.print("Vui lòng nhập số n vào: ");
+        int n = sc.nextInt();
 
-        System.out.print("Vui lòng nhập số x vào: ");
-        x = sc.nextDouble();
-
-        double result = tinhTong(n, x);
-
-        System.out.println("Tổng của n = " + n + " và x = " + x + " là: " + result);
+        long result = tinhGiaiThua(n);
+        System.out.println("Giai thừa " + n + "! là: " + result);
     }
 }
