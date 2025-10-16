ccfd682ef6b350fc76653a7130d7f8da2229bf0d
Taam-cr <tranphanthaotam2003@gmail.com>
2025-10-17 03:44:51 +0700

    Tong cac so le

--- VongLap/src/While.java
+++ VongLap/src/While.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+
 public class While {
     public static void main(String[] args) {
-        int n = 0;
+        Scanner sc = new Scanner(System.in);
+        int n;
+        do {
+            System.out.print("Nhập số nguyên dương lớn hơn 0: ");
+            n = sc.nextInt();
+            if (n <= 0) {
+                System.out.println("Vui lòng nhập lại");
+            }
+        } while (n <= 0);
         int sum = 0;
-
-        while (sum <= 10000) {
-            n++;
-            sum += n;
+        for (int i = 1; i < n; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
         }
 
-        System.out.println("Số nhỏ nhất thỏa điều kiện là: " + n);
+        System.out.println("Tổng các số bé hơn " + n + " là: " + sum);
     }
 }
