63ce520f316d9d16c040256b75149ba14688679d
Taam-cr <tranphanthaotam2003@gmail.com>
2025-10-17 03:55:34 +0700

    Tinh so nam doi

--- VongLap/src/While.java
+++ VongLap/src/While.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class While {
-
-    public static long tinhGiaiThua(int n) {
-        long giaiThua = 1;
-        for (int i = 1; i <= n; i++) {
-            giaiThua *= i;
-        }
-        return giaiThua;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Vui lòng nhập số n vào: ");
-        int n = sc.nextInt();
+        System.out.print("Vui lòng nhập số tiền gửi (triệu) vào: ");
+        double tienGoc = sc.nextDouble();
 
-        long result = tinhGiaiThua(n);
-        System.out.println("Giai thừa " + n + "! là: " + result);
+        System.out.print("Vui lòng nhập số tiền mong muốn (triệu) vào: ");
+        double tienMongMuon = sc.nextDouble();
+
+        System.out.print("Vui lòng nhập lãi suất gửi (%) từ 0 - 100 vào: ");
+        double laiSuat = sc.nextDouble();
+
+        int nam = 0;
+        double tienHienTai = tienGoc;
+
+        while (tienHienTai < tienMongMuon) {
+            tienHienTai += tienHienTai * laiSuat / 100; 
+            nam++;
+        }
+
+        System.out.println("Số năm đợi chờ là: " + nam);
     }
 }
